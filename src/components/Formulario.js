449f4bc4a4617622d33a89c3e422297af911c29d
Martin Becchio <tincho.becchio@gmail.com>
2023-04-02 13:25:38 -0300

    creando custom hook para el select

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,21 @@
 import styles from './Formulario.module.css'
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+    const [ categoria, SelectNoticias] = useSelect()
+
+
+
+
+
     return (
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
                 <form>
                     <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+                    <SelectNoticias />
 
                     <div className="input-field col s12">
                         <input 
