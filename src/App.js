730d8bb9d807f96062258458c05c32c7831c4675
Martin Becchio <tincho.becchio@gmail.com>
2023-04-02 12:45:23 -0300

    agregando materialize, fuente de google y creando el header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <h1>Noticias</h1>
+    <>
+      <Header 
+        titulo="Buscador de Noticias"
+      />
+      <div className="container white">
+        <h1>Formulario aqui</h1>
+      </div>
+    </>
   );
 }
 
